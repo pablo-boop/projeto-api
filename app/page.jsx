1b1c3cdeae23f7ccbdf59a4fcc4e8d19e9b96d03
Pablo Couto <pablocouto@aluno.senai.br>
2023-10-17 16:43:20 -0300

    feat: alterações page

--- app/page.jsx
+++ app/page.jsx
@@ -4,67 +4,67 @@ import rickAndMorty, { alterPage } from "@/data/RickAndMorty";
 import styles from './page.module.css'
 
 export default function Home() {
-  const [data, setData] = useState(null)
-  const [page, setPage] = useState(1)
+    const [data, setData] = useState(null)
+    const [page, setPage] = useState(1)
 
-  const addPage = () => {
-      setPage(page + 1)
-  }
+    const addPage = () => {
+        setPage(page + 1)
+    }
 
-  const removePage = () => {
-      page ? (
-          page <= 1,
-          setPage(1)
-      ) : (
-          setPage(page - 1)
-      )
-  }
+    const removePage = () => {
+        page ? (
+            page <= 1,
+            setPage(1)
+        ) : (
+            setPage(page - 1)
+        )
+    }
 
-  const changePage = () => {
-      alterPage(page)
-  }
+    const changePage = () => {
+        alterPage(page)
+    }
 
-  useEffect(() => {
-      const mortyFetch = async() => {
-          try {
-              const dados = await rickAndMorty();
-              setData(dados)
-              console.log(data);
-          } catch (error) {
-              throw error
-          }
-      };
-      mortyFetch();
-  }, [])
+    useEffect(() => {
+        const mortyFetch = async () => {
+            try {
+                const dados = await rickAndMorty();
+                setData(dados)
+                console.log(data);
+            } catch (error) {
+                throw error
+            }
+        };
+        mortyFetch();
+    }, [])
 
-  return (
-      <div>
-          <div className={styles.cards}>
-              {
-                  data.map((character) => (
-                      <div key={character.id}>
-                          <div>
-                              <img src={character.image} alt={character.name} />
-                          </div>
-                          <h3>{character.name}</h3>
-                          <p>{character.status}</p>
-                          <p>{character.species}</p>
-                          <p>{character.origin.name}</p>
-                      </div>
-                  ))
-              }
-              <div className={styles.buttons}>
-                  <button onClick={() => {
-                      removePage();
-                      changePage();
-                  }}> ⬅ </button>
+    return (
+        <div>
+            <div className={styles.cards}>
+                {
+                    data.map((character) => (
+                        <div key={character.id}>
+                            <div>
+                                <img src={character.image} alt={character.name} />
+                            </div>
+                            <h3>{character.name}</h3>
+                            <p>{character.status}</p>
+                            <p>{character.species}</p>
+                            <p>{character.origin.name}</p>
+                        </div>
+                    ))
+                }
+                <div className={styles.buttons}>
+                    <button onClick={() => {
+                        removePage();
+                        changePage();
+                    }}> ⬅ </button>
 
-                  <button onClick={() => {
-                      addPage();
-                      changePage();
-                  }}> ➡ </button>
-              </div>
-          </div>
-      </div>
-  )
+                    <button onClick={() => {
+                        addPage();
+                        changePage();
+                    }}> ➡ </button>
+                </div>
+            </div>
+        </div>
+    )
 }
